8d9b6b91ff6d54c9e1be3de1eae23485dca45b9a
david <dmhernandezt0@gmail.com>
2023-04-28 17:45:05 -0700

    first

---
+++ RockMusic/Band.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockMusic
+{
+    public class Band
+    {
+        public string bandName;
+        public string singer;
+        public string guitarist;
+        public string bassist;
+        public string drummer;
+        
+        public Band()
+        {
+            return;
+        }
+
+        public Band(string name, string member1, string member2, string member3, string member4)
+        {
+            this.bandName = name;
+            this.singer = member1;
+            this.guitarist = member2;
+            this.bassist = member3;
+            this.drummer = member4;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("Band Name: {0}\nSinger: {1}\nGuitarist: {2}\nBassist: {3}\nDrummer: {4}\n",bandName, singer, guitarist, bassist, drummer);
+        }
+
+ 
+    }
+}
---
+++ RockMusic/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockMusic
+{
+    public class RockMusic
+    {
+        public Band band;
+        public RockMusic()
+        {
+            band = new Band();
+        }
+
+        static void Main(string[] args)
+        {
+
+            RockMusic genre = new RockMusic();
+
+            genre.band.bandName = "Van Halen";
+
+            Band beatles = new Band();
+            Band ledZeppelin = new Band();
+
+            beatles.Info();
+
+            genre.band.Info();
+        }
+    }
+
+    
+}
